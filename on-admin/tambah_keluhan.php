83d12a9db33d740911f19a4629ee707e0a2eb7c5
saepulfariz <saepulfariz108@gmail.com>
2024-10-17 20:36:25 +0700

    change new pelanggan with input keluhan

--- on-admin/tambah_keluhan.php
+++ on-admin/tambah_keluhan.php
@@ -1,16 +1,28 @@
 <?php
-	include('../config.php');
-	$idpelanggan=$_POST['idpelanggan'];
-	$keluhan=$_POST['keluhan'];
-	$penyebab=$_POST['penyebab'];
-	$tindakan=$_POST['tindakan'];
-	$tgl_keluhan=$_POST['tgl_keluhan'];
-	$tgl_perbaikan=$_POST['tgl_perbaikan'];
-	$idteknisi=$_POST['idteknisi'];
-	
-	mysqli_query($conn, "insert into keluhan (idpelanggan, keluhan, penyebab, tindakan, tgl_keluhan, tgl_perbaikan, idteknisi) 
+include('../config.php');
+
+$nama_pelanggan = $_POST['nama_pelanggan'];
+$alamat_pelanggan = $_POST['alamat_pelanggan'];
+$telepon_pelanggan = $_POST['telepon_pelanggan'];
+$email_pelanggan = $_POST['email_pelanggan'];
+
+$insert = mysqli_query($conn, "insert into pelanggan (nama_pelanggan, alamat_pelanggan, telepon_pelanggan, email_pelanggan) 
+							values 
+							('$nama_pelanggan', '$alamat_pelanggan', '$telepon_pelanggan', '$email_pelanggan')");
+
+$query = "SELECT idpelanggan FROM pelanggan ORDER BY idpelanggan DESC";
+$sql = mysqli_query($conn, $query);
+$row = mysqli_fetch_assoc($sql);
+
+$idpelanggan = $row['idpelanggan'];
+$keluhan = $_POST['keluhan'];
+$penyebab = $_POST['penyebab'];
+$tindakan = $_POST['tindakan'];
+$tgl_keluhan = $_POST['tgl_keluhan'];
+$tgl_perbaikan = $_POST['tgl_perbaikan'];
+$idteknisi = $_POST['idteknisi'];
+
+mysqli_query($conn, "insert into keluhan (idpelanggan, keluhan, penyebab, tindakan, tgl_keluhan, tgl_perbaikan, idteknisi) 
 							values 
 							('$idpelanggan', '$keluhan', '$penyebab', '$tindakan', '$tgl_keluhan', '$tgl_perbaikan', '$idteknisi')");
-	header('location:keluhan.php');
-
-?>
+header('location:keluhan.php');
